bb6c2b9c81bbeb760ed984d24c8999a63517a0a1
Aileen Chan <achan@ai-t.com.au>
2024-10-22 21:06:51 +1100

    2nd Line of Code to JavaTest

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello out there!!!");
+        System.out.println("Testing 2nd Line of Code");
     }
 
 }
